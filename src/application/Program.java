6da862cd3defa8c7831f791f9060ff349b652f14
fabiogrod <fabiogrimello@gmail.com>
2020-10-24 14:13:44 -0300

    PQP cuzao

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,5 +7,7 @@ public class Program {
 		System.out.println("Tá de capotar o corsa!!");
 		System.out.println("Tá de cair o cu da bunda!!");
 		System.out.println("Tá de fuder Bahia!!");
+		
+		System.out.println("Vaza");
 	}
 }
